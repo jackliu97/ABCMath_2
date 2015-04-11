97bb923e977a63903bdf8142bff0b451eaf3178f
jackliu97 <jackliu97@gmail.com>
2015-04-11 09:14:28 -0400

    added em to connections

--- lib/ABCMath/Db/Connection.php
+++ lib/ABCMath/Db/Connection.php
@@ -3,11 +3,25 @@ namespace ABCMath\Db;
 
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 
 class Connection
 {
     protected static $_config = null;
     protected static $_conn = null;
+    protected static $_em = null;
+
+    public static function config(){
+        return $connectionParams = array(
+                'dbname'    => DB_DATABASE,
+                'user'      => DB_USERNAME,
+                'password'  => DB_PASSWORD,
+                'host'      => DB_HOSTNAME,
+                'port'      => DB_PORT,
+                'driver'    => 'pdo_mysql',
+            );
+    }
 
     public static function getConnection()
     {
@@ -17,17 +31,28 @@ class Connection
 
         if (is_null(self::$_config)) {
             self::$_config = new Configuration();
-            $connectionParams = array(
-                'dbname'    => DB_DATABASE,
-                'user'      => DB_USERNAME,
-                'password'  => DB_PASSWORD,
-                'host'      => DB_HOSTNAME,
-                'port'      => DB_PORT,
-                'driver'    => 'pdo_mysql',
-            );
+            $connectionParams = self::config();
         }
         self::$_conn = DriverManager::getConnection($connectionParams, self::$_config);
 
         return self::$_conn;
+    }
+
+    public static function getEm()
+    {
+
+        if (!is_null(self::$_em)) {
+            return self::$_em;
+        }
+
+        $isDevMode = true;
+        //$paths = array(__DIR__ . '/mappings/entities');
+        //$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+
+        $paths = array(__DIR__ . '/mappings/xml');
+        $config = Setup::createXMLMetadataConfiguration($paths, $isDevMode);
+        self::$_em = EntityManager::create(self::config(), $config);
+
+        return self::$_em;
     }
 }
